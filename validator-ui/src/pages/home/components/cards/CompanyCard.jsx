a2df7c16a0dc3a3517f9cbdac9a03564c150917a
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-06-15 15:53:42 +0300

    fix: update company deletion logic to use company object instead of companyId

--- validator-ui/src/pages/home/components/cards/CompanyCard.jsx
+++ validator-ui/src/pages/home/components/cards/CompanyCard.jsx
@@ -78,7 +78,7 @@ export function CompanyCard({
         };
 
         // remove the company
-        const response = await post(routes.COMPANY_DELETE, { companyId: id });
+        const response = await post(routes.COMPANY_DELETE, { company });
         if (response.status !== 200) {
             setAlert('A aparut o eroare la stergerea companiei.', 'error');
             return;
